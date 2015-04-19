0fd4cdf249c742ffa30c63eb2c6566e021641b8a
jeremyfa <jeremy.faivre@gmail.com>
2015-04-20 01:59:38 +0200

    Improve game logic (paper frequency and position)

--- src/Main.hx
+++ src/Main.hx
@@ -21,20 +21,29 @@ class Main extends luxe.Game {
     var just_pressed_space_key: Bool;
 
     var current_pencil: Pencil;
-    var current_pencil_speed: Float = 190.0 * 3;
-    var current_pencil_initial_x: Float = -250;
+    var current_pencil_speed: Float = 240.0 * 1.5;
+    var current_pencil_initial_x: Float = -100;
     var current_pencil_final_x: Float = 0;
 
+    var drawing_pencil_delay: Float = 0.1;
+
     var thrown_pencils: Array<Pencil> = [];
-    var thrown_pencil_speed: Float = 240.0 * 2;
+    var thrown_pencil_speed: Float = 240.0 * 1;
 
     var paper_half_width: Float = 250.0;
-    var papers_speed: Float = 200.0 * 2;
+    var papers_speed: Float = 200.0 * 1;
     var papers: Array<Paper> = [];
     var last_paper_sub_depth: Float = 0.0;
+    var next_paper_on_same_row_delay: Float = 5.0;
+    var new_paper_frequency: Float = 0.025;
 
     var pencil_half_width: Float = 100;
     var pencil_half_height: Float = 10;
+
+    var used_paper_rows: Map<Int,Bool> = new Map<Int,Bool>();
+
+    var background1: Sprite;
+    var background2: Sprite;
 
     override function ready() {
             // Init preload
@@ -50,6 +59,7 @@ class Main extends luxe.Game {
         preload.add_texture("assets/paper.png");
         preload.add_texture("assets/pencil_default.png");
         preload.add_texture("assets/pencil_writing.png");
+        preload.add_texture("assets/background.png");
 
             // Progress bar
         new ParcelProgress({
@@ -65,24 +75,45 @@ class Main extends luxe.Game {
 
 
     function assets_loaded(_) {
-            // Initialize scene
-        init_scene();
             // Connect input
         connect_input();
+            // Initialize scene
+        init_scene();
 
     } //assets_loaded
 
 
     function init_scene() {
+        Luxe.loadTexture("assets/background.png").filter = FilterType.nearest;
+
             // Create background
-        Luxe.draw.box({
-            w:      Luxe.screen.w,
-            h:      Luxe.screen.h,
-            pos:    Luxe.screen.mid,
-            depth:  1,
-            color:  new Color().rgb(0x777777)
+        background1 = new Sprite({
+            pos:        Luxe.screen.mid,
+            texture:    Luxe.loadTexture("assets/background.png"),
+            depth:      1.0001
+        });
+        background2 = new Sprite({
+            pos:        new Vector(Luxe.screen.mid.x + Luxe.screen.w * 0.5, Luxe.screen.mid.y),
+            texture:    Luxe.loadTexture("assets/background.png"),
+            depth:      1.0002
         });
 
+            // Increase speed every 10 second
+        var level = 0;
+        Luxe.timer.schedule(10, function() {
+            if (level < 14) {
+                current_pencil_speed *= 1.1;
+                thrown_pencil_speed *= 1.1;
+                papers_speed *= 1.1;
+                new_paper_frequency *= 1.1;
+                drawing_pencil_delay /= 1.1;
+                next_paper_on_same_row_delay /= 1.1;
+
+                trace(level++);
+            }
+        }, true);
+
+            // Create first pencil
         use_new_pencil();
 
     } //init_scene
@@ -123,11 +154,11 @@ class Main extends luxe.Game {
     } //create_new_pencil
 
 
-    function use_drawing_pencil(thrown_pencil:Pencil) {
+    function use_drawing_pencil(thrown_pencil:Pencil, paper:Paper) {
             // Get thrown pencil position
         var pos = thrown_pencil.pos;
             // Ajust position from sprite change
-        pos.y -= 40;
+        pos.y = paper.pos.y - 35;
 
             // Remove throw pencil
         thrown_pencils.remove(thrown_pencil);
@@ -141,22 +172,23 @@ class Main extends luxe.Game {
 
             // Do some animation (move the pencil up and down)
         var initial_y = pos.y;
-        var step = 3.0;
-        var timer;
 
-        Actuate.timer(0.1).onUpdate(function() {
+        var step = 0.3 / drawing_pencil_delay;
+        var delay = drawing_pencil_delay;
+
+        Actuate.timer(delay).onUpdate(function() {
             drawing_pencil.pos.y += step;
         }).onComplete(function() {
-            Actuate.timer(0.1).onUpdate(function() {
+            Actuate.timer(delay).onUpdate(function() {
                 drawing_pencil.pos.y -= step;
             }).onComplete(function() {
-                Actuate.timer(0.05).onUpdate(function() {
+                Actuate.timer(delay).onUpdate(function() {
                     drawing_pencil.pos.y += step;
                 }).onComplete(function() {
-                    Actuate.timer(0.1).onUpdate(function() {
+                    Actuate.timer(delay).onUpdate(function() {
                         drawing_pencil.pos.y -= step;
                     }).onComplete(function() {
-                        Actuate.tween(drawing_pencil.color, 0.25, {a: 0}).onComplete(function() {
+                        Actuate.tween(drawing_pencil.color, delay * 2.5, {a: 0}).onComplete(function() {
                             drawing_pencil.destroy();
                         });
                     });
@@ -169,11 +201,27 @@ class Main extends luxe.Game {
 
     function add_paper() {
 
+            // Compute the best row
+        var row:Int = Math.floor(Math.random() * 4.99999);
+        var number_of_used_rows:Int = 0;
+        for (key in used_paper_rows) {
+            number_of_used_rows++;
+        }
+        if (number_of_used_rows >= 5) return;
+        while (used_paper_rows.exists(row)) {
+            row = Math.round(Math.random() * 4.99999);
+        }
+        used_paper_rows.set(row, true);
+        Luxe.timer.schedule(next_paper_on_same_row_delay, function() {
+            used_paper_rows.remove(row);
+        });
+
+            // Create paper
         var paper = new Paper({
-            pos:        new Vector(Luxe.screen.w + paper_half_width, Math.round(Luxe.screen.h * Luxe.utils.random.get())),
+            pos:        new Vector(Luxe.screen.w + paper_half_width, Math.round(Luxe.screen.h * 0.1 + Luxe.screen.h * 0.8 * row / 5.0)),
             depth:      2 + last_paper_sub_depth,
             rotation_z: Math.round(Luxe.utils.random.float(-90, 90)),
-            censored:   Luxe.utils.random.bool()
+            censored:   (Math.random() > 0.25)
         });
 
             // Increase depth a little bit to be sure papers
@@ -199,7 +247,7 @@ class Main extends luxe.Game {
             // just move it a bit more to the right
         if (current_pencil.pos.x < current_pencil_final_x) {
                 // Update X
-            current_pencil.pos.x = Math.min(current_pencil.pos.x + dt * current_pencil_speed * 2, current_pencil_final_x);
+            current_pencil.pos.x = Math.min(current_pencil.pos.x + dt * thrown_pencil_speed, current_pencil_final_x);
 
                 // Allow pressing space only when the pencil is at its final X position
             just_pressed_space_key = false;
@@ -230,7 +278,7 @@ class Main extends luxe.Game {
         }
 
             // Add paper?
-        if (Luxe.utils.random.bool(0.02)) {
+        if (Math.random() < new_paper_frequency) {
             add_paper();
         }
 
@@ -254,6 +302,16 @@ class Main extends luxe.Game {
                 papers.remove(paper);
                 paper.destroy();
             }
+        }
+
+            // Update background position
+        background1.pos.x -= dt * papers_speed;
+        background2.pos.x -= dt * papers_speed;
+        if (background1.pos.x < 0) {
+            background1.pos.x += Luxe.screen.w;
+        }
+        if (background2.pos.x < 0) {
+            background2.pos.x += Luxe.screen.w;
         }
 
             // Detect collisions
@@ -280,7 +338,7 @@ class Main extends luxe.Game {
                     paper.draw_on_paper(Std.int(Math.min(5, Math.floor(1 + Math.random() * 5))));
 
                         // And use drawing pencil
-                    use_drawing_pencil(thrown_pencil);
+                    use_drawing_pencil(thrown_pencil, paper);
                     break;
                 }
             }
